2f3b94af8c409b4674a22dc9496b1145d7dce6e7
Esteban Sayago <sayagoo823@gmail.com>
2024-05-15 21:00:43 -0300

    feature: pagina jugar, agregar logica y estilos

--- js/jugar.js
+++ js/jugar.js
@@ -1,43 +1,33 @@
-// const mock = (object = {}) => {
-//   return {
-//     name: object.name.common,
-//     capital: object.capital ? object.capital[0] : "",
-//     flag: object.flags.svg,
-//   };
-// };
+const fraseAlMostrarPais = document.getElementById("frase");
+const imagen = document.getElementById("img-flag");
+const options = document.querySelectorAll(".option");
+const btnReiniciar = document.getElementById("btnReiniciar");
+const resultInfo = document.querySelector(".result-info");
+const levels = document.querySelectorAll(".level");
+const boxResult = document.querySelector(".result");
+
+const mock = (object = {}) => {
+  return {
+    name: object.name.common,
+    capital: object.capital ? object.capital[0] : "",
+    flag: object.flags.svg,
+  };
+};
+
+const frasesRandoms = [
+  "Que bandera es esta mi rey?",
+  "Y esta bandera mi loco?",
+  "a ver decime el nombre de esta banderita:",
+  "Diez pe que no le acertas el nombre:",
+  "malo si le erras a esta:",
+  "opa opa, me dicen por ahi que no sabes el nombre de esta banderita: ",
+];
 
 // const data = await fetch("https://restcountries.com/v3.1/all").then((d) =>
 //   d.json()
 // );
 // const c = data.map((d) => mock(d));
 
-const localData = [
-  { name: "Moldova", capital: "Chișinău", flag: "https://flagcdn.com/md.svg" },
-  {
-    name: "United States",
-    capital: "Washington, D.C.",
-    flag: "https://flagcdn.com/us.svg",
-  },
-  { name: "Mayotte", capital: "Mamoudzou", flag: "https://flagcdn.com/yt.svg" },
-  { name: "Nauru", capital: "Yaren", flag: "https://flagcdn.com/nr.svg" },
-  { name: "Mozambique", capital: "Maputo", flag: "https://flagcdn.com/mz.svg" },
-  { name: "Brazil", capital: "Brasília", flag: "https://flagcdn.com/br.svg" },
-  { name: "Cape Verde", capital: "Praia", flag: "https://flagcdn.com/cv.svg" },
-  {
-    name: "Equatorial Guinea",
-    capital: "Malabo",
-    flag: "https://flagcdn.com/gq.svg",
-  },
-  { name: "Albania", capital: "Tirana", flag: "https://flagcdn.com/al.svg" },
-  {
-    name: "United States Virgin Islands",
-    capital: "Charlotte Amalie",
-    flag: "https://flagcdn.com/vi.svg",
-  },
-  { name: "Niue", capital: "Alofi", flag: "https://flagcdn.com/nu.svg" },
-];
-
-// Definimos un array de objetos con la información de las banderas y sus países
 const paises = [
   { name: "Moldova", capital: "Chișinău", flag: "https://flagcdn.com/md.svg" },
   {
@@ -61,65 +51,110 @@ const paises = [
     capital: "Charlotte Amalie",
     flag: "https://flagcdn.com/vi.svg",
   },
-  { name: "Niue", capital: "Alofi", flag: "https://flagcdn.com/nu.svg" },
 ];
 
-// Función para obtener un conjunto aleatorio de países para cada nivel
-function obtenerPaisesAleatorios(numeroPaises) {
-  const paisesAleatorios = [];
-  while (paisesAleatorios.length < numeroPaises) {
-    const indiceAleatorio = Math.floor(Math.random() * paises.length);
-    if (!paisesAleatorios.includes(indiceAleatorio)) {
-      paisesAleatorios.push(indiceAleatorio);
-    }
-  }
-  return paisesAleatorios;
+function numeroRandom(rango) {
+  return Math.trunc(Math.random() * rango);
 }
 
-// Función para iniciar un nivel de juego
-function iniciarNivel(nivel) {
-  const paisesAleatorios = obtenerPaisesAleatorios(4); // Obtenemos 4 países aleatorios para mostrar
-  const respuestaCorrecta = Math.floor(Math.random() * 4); // Índice de la respuesta correcta
+let nivelInicial = 1;
+let respuestaRandom = "";
+let indicesAcertados = [];
+let indiceCorrecto = 0;
+let indicesMostrados = [];
 
-  console.log(`Nivel ${nivel}`);
-  console.log("Adivina la bandera:");
+function obtenerIndicesDePaisesAleatorios() {
+  const indicesAleatorios = [];
+  const generarIndices = () => {
+    const indiceRandom = numeroRandom(paises.length);
+    if (indicesAleatorios.length === 4) {
+      if (indicesAleatorios.every((n) => indicesMostrados.includes(n))) {
+        const indicesNoMostrados = paises.filter(
+          (n, i) => !indicesMostrados.includes(i)
+        );
+        indicesAleatorios[0] = paises.indexOf(indicesNoMostrados[0]);
+        return;
+      }
+      return;
+    }
 
-  // Mostramos las opciones al jugador
-  paisesAleatorios.forEach((indice, i) => {
-    console.log(`${i + 1}. ${paises[indice].name}`);
-  });
+    if (!indicesAleatorios.includes(indiceRandom)) {
+      indicesAleatorios.push(indiceRandom);
+    }
+    return generarIndices();
+  };
+  generarIndices();
+  return indicesAleatorios;
+}
 
-  // Lógica para verificar la respuesta del jugador
-  const respuestaJugador = parseInt(
-    prompt("Ingresa el número de la opción correcta:")
+function respuestaAleatoria(paisesAleatorios) {
+  const indicesNoMostrados = paisesAleatorios.filter(
+    (p) => !indicesMostrados.includes(p)
   );
 
-  if (respuestaJugador === respuestaCorrecta + 1) {
-    console.log("¡Respuesta correcta!");
-    return true; // El jugador acertó
-  } else {
-    console.log(
-      `Respuesta incorrecta. La respuesta correcta era ${
-        respuestaCorrecta + 1
-      }.`
-    );
-    return false; // El jugador falló
-  }
+  return indicesNoMostrados[numeroRandom(indicesNoMostrados.length)];
 }
 
-// Función principal para iniciar el juego
-function iniciarJuego() {
-  let puntos = 0;
-  const niveles = 10;
-
-  for (let i = 1; i <= niveles; i++) {
-    if (iniciarNivel(i)) {
-      puntos++;
+function nuevoNivel(e) {
+  const valorSeleccionado = e.target.textContent;
+  if (valorSeleccionado === respuestaRandom) {
+    if (!indicesAcertados.includes(indiceCorrecto)) {
+      indicesAcertados.push(indiceCorrecto);
     }
   }
 
-  console.log(`Juego terminado. Obuviste ${puntos} puntos.`);
+  if (nivelInicial === 10) {
+    resultInfo.innerHTML = `
+    <p>Banderas acertadas: ${indicesAcertados.length} / 10</p>
+    <p>Nombres de las banderas acertadas: ${
+      indicesAcertados.map((e) => paises[e].name).join(", ") || 0
+    } </p>
+    <p>Banderas erradas: ${
+      indicesMostrados
+        .filter((e) => !indicesAcertados.includes(e))
+        .map((e) => paises[e].name)
+        .join(", ") || 0
+    } </p>
+    `;
+    boxResult.style.display = "block";
+    return;
+  }
+  nivelInicial++;
+  iniciarNivel();
 }
 
-// // Iniciamos el juego
-// iniciarJuego();
+function iniciarNivel() {
+  const paisesAleatorios = obtenerIndicesDePaisesAleatorios();
+  const respuestaCorrecta = respuestaAleatoria(paisesAleatorios);
+  const fraseRandom = frasesRandoms[numeroRandom(frasesRandoms.length)];
+
+  indicesMostrados.push(respuestaCorrecta);
+  respuestaRandom = paises[respuestaCorrecta].name;
+  indiceCorrecto = respuestaCorrecta;
+
+  frase.innerHTML = fraseRandom;
+  imagen.src = paises[respuestaCorrecta].flag;
+
+  paisesAleatorios.forEach((indice, i) => {
+    options[i].innerHTML = paises[indice].name;
+    options[i].addEventListener("click", nuevoNivel);
+  });
+  console.log(nivelInicial);
+  levels[nivelInicial - 1].classList.toggle("active-phase");
+  levels[nivelInicial - 2]?.classList.toggle("checked");
+}
+
+function reiniciarJuego() {
+  nivelInicial = 1;
+  respuestaRandom = "";
+  indicesAcertados = [];
+  indiceCorrecto = 0;
+  indicesMostrados = [];
+  resultInfo.innerHTML = "";
+  levels.forEach((e) => e.classList.remove("checked", "active-phase"));
+  boxResult.style.display = "none";
+  iniciarNivel();
+}
+
+btnReiniciar.addEventListener("click", reiniciarJuego);
+iniciarNivel();
